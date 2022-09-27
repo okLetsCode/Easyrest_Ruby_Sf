241b28d247ca1ba71fc0e1bd311730fa5629c3dc
desislava453 <desislava453@gmail.com>
2022-09-27 19:09:19 +0300

    Created config files for creds and env, shared_objects and rubocop config

--- pages/shared_objects.rb
+++ pages/shared_objects.rb
@@ -2,4 +2,23 @@
 
 # This is class SharedObjects
 class SharedObjects
+  ALL_TAB = { xpath: "//*[contains(text(),'All ')]" }.freeze
+  ACTIVE_TAB = { xpath: "//*[contains(text(),'Active ')]" }.freeze
+  BANNED_TAB = { xpath: "//*[contains(text(),'Banned ')]" }.freeze
+
+  def initialize(driver)
+    @driver = driver
+  end
+
+  def all_tab_click
+    @driver.find_element(ALL_TAB).click
+  end
+
+  def active_tab_click
+    @driver.find_element(ACTIVE_TAB).click
+  end
+
+  def banned_tab_click
+    @driver.find_element(BANNED_TAB).click
+  end
 end
