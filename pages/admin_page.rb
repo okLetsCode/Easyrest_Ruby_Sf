27d2608eff9aa85ca0f5ee3ae714dbb7a59976c8
zdravko <ziliev6@gmail.com>
2022-09-28 13:26:06 +0300

    admin page

--- pages/admin_page.rb
+++ pages/admin_page.rb
@@ -1,35 +1,31 @@
-#This class is for admin page
-class AdminPage
-  USER_ADMIN_PROFILE_BTN = { xpath: '/html/body/div/header/div/div/div/button/span[1]/div' }
-  ADMIN_PANEL = { css: '[href*="/admin"]' }
-  USERS_BTN = { css: '[href*=/admin/users]' }
-  USER_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button/span[1]/svg/path[2]' }
+# frozen_string_literal: true
 
-  OWNERS_BTN = { xpath: '/html/body/div/div/ul/a[2]' } 
-  OWNERS_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button' }
-  
-  MODERATORS_BTN = { css: '[href*="/admin/moderators"]' }
-  MODERATORS_ALL = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[1]/span[1]/span/span' }
-  MODERATORS_ACTIVE = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[2]/span[1]/span/span'}
-  MODERATORS_BANNED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[3]/span[1]/span/span'}
-  ADD_MODERATOR_BTN = { xpath: '/html/body/div/div/main/a' }
-  MODERATOR_CANCEL_BTN = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[7]/div/a' }
-  ADD_MODERATOR_NAME = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[1]/div/div/input' }
-  ADD_MODERATOR_EMAIL = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[2]/div/div/input'}
-  ADD_MODERATOR_PHONENUMBER = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[3]/div/div/input'}
-  ADD_MODERATOR_BIRTHDATE = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[4]/div/div[1]/div/div/input' }
-  ADD_MODERATOR_PASS = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[5]/div/div/input'}
-  ADD_MODERATOR_CONFIRMPASS = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[6]/div/div/input' }
-  ADD_MODERATOR_CREATEACC = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[7]/div/button' }
-  MODERATORS_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr/td[5]/button' }
-  
-  RESTAURANTS_BTN = { xpath: '/html/body/div/div/ul/a[4]/div[2]' }
-  RESTAURANTS_UNAPPROVED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[2]/span[1]/span' }
-  RESTAURANTS_APPROVED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[3]/span[1]/span' }
-  
-  
-  
-  
+# This class is for AdminPage
+class AdminPage
+  USER_ADMIN_PROFILE_BTN = { xpath: '/html/body/div/header/div/div/div/button/span[1]/div' }.freeze
+  ADMIN_PANEL = { css: '[href*="/admin"]' }.freeze
+  USERS_BTN = { css: '[href*=/admin/users]' }.freeze
+  USER_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button/span[1]/svg/path[2]' }.freeze
+  OWNERS_BTN = { xpath: '/html/body/div/div/ul/a[2]' }.freeze
+  OWNERS_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr[1]/td[5]/button' }.freeze
+  MODERATORS_BTN = { css: '[href*="/admin/moderators"]' }.freeze
+  MODERATORS_ALL = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[1]/span[1]/span/span' }.freeze
+  MODERATORS_ACTIVE = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[2]/span[1]/span/span' }.freeze
+  MODERATORS_BANNED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[3]/span[1]/span/span' }.freeze
+  ADD_MODERATOR_BTN = { xpath: '/html/body/div/div/main/a' }.freeze
+  MODERATOR_CANCEL_BTN = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[7]/div/a' }.freeze
+  ADD_MODERATOR_NAME = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[1]/div/div/input' }.freeze
+  ADD_MODERATOR_EMAIL = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[2]/div/div/input' }.freeze
+  ADD_MODERATOR_PHONENUMBER = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[3]/div/div/input' }.freeze
+  ADD_MODERATOR_BIRTHDATE = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[4]/div/div[1]/div/div/input' }.freeze
+  ADD_MODERATOR_PASS = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[5]/div/div/input' }.freeze
+  ADD_MODERATOR_CONFIRMPASS = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[6]/div/div/input' }.freeze
+  ADD_MODERATOR_CREATEACC = { xpath: '/html/body/div/div/main/div/div/form/div/div/div[7]/div/button' }.freeze
+  MODERATORS_LOCK = { xpath: '/html/body/div/div/main/div[2]/table/tbody/tr/td[5]/button' }.freeze
+  RESTAURANTS_BTN = { xpath: '/html/body/div/div/ul/a[4]/div[2]' }.freeze
+  RESTAURANTS_UNAPPROVED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[2]/span[1]/span' }.freeze
+  RESTAURANTS_APPROVED = { xpath: '/html/body/div/div/main/div[1]/header/div/div[2]/div[2]/div/button[3]/span[1]/span' }.freeze
+
   def initialize(driver)
     @driver = driver
   end
@@ -44,7 +40,6 @@ class AdminPage
 
   def click_users_btn
     driver.find_element(USERS_BTN).click
-  
   end
 
   def click_unlock_user_btn
@@ -59,23 +54,26 @@ class AdminPage
     driver.find_element(OWNERS_LOCK).click
   end
 
-
-
   def click_moderators_btn
     driver.find_element(MODERATORS_BTN).click
   end
+
   def click_moderators_all
     driver.find_element(MODERATORS_ALL).click
   end
+
   def click_moderators_active
     driver.find_element(MODERATORS_ACTIVE).click
   end
+
   def click_moderators_banned
     driver.find_element(MODERATORS_BANNED).click
   end
+
   def click_moderators_lock
     driver.find_element(MODERATORS_LOCK).click
   end
+
   def click_moderators_cancel
     driver.find_element(MODERATORS_CANCEL_BTN).click
   end
@@ -83,35 +81,44 @@ class AdminPage
   def click_add_moderators
     driver.find_element(ADD_MODERATOR_BTN).click
   end
+
   def click_add_moderators_birthdate
     driver.find_element(ADD_MODERATOR_BIRTHDATE).click
   end
+
   def click_add_moderators_confirmpass
     driver.find_element(ADD_MODERATOR_CONFIRMPASS).click
   end
+
   def click_add_moderators_createacc
     driver.find_element(ADD_MODERATOR_CREATEACC).click
   end
+
   def click_add_moderators_email
     driver.find_element(ADD_MODERATOR_EMAIL).click
   end
+
   def click_add_moderators_name
     driver.find_element(ADD_MODERATOR_NAME).click
   end
+
   def click_add_moderators_pass
     driver.find_element(ADD_MODERATOR_PASS).click
   end
+
   def click_add_moderators_phonenumber
     driver.find_element(ADD_MODERATOR_PHONENUMBER).click
   end
 
-
   def click_restaurants_btn
     driver.find_element(RESTAURANTS_BTN).click
   end
+
   def click_restaurants_approved
     driver.find_element(RESTAURANTS_APPROVED).click
   end
+
   def click_restaurants_unapproved
     driver.find_element(RESTAURANTS_UNAPPROVED).click
   end
+end
