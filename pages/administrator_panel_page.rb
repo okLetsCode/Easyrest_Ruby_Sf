150cf437fd8d01c30629b98b535a4b9558c1a559
Ivaylo Petkov <ivaylo300@gmail.com>
2022-10-11 18:34:00 +0300

    xpaths, and other files

--- pages/administrator_panel_page.rb
+++ pages/administrator_panel_page.rb
@@ -2,14 +2,14 @@
 
 # This page contains class Administrator and the page objects for it.
 class Administrator
-  ROW = { xpath: '/html/body/div/main/div/div/div/div/div[1]/div[1]' }.freeze
-  ACCEPT_BTN = { xpath: '/html/body/div/main/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div/button/span[1]' }.freeze
-  ASSIGN_BTN = { xpath: '/html/body/div/main/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div[3]/button' }.freeze
-  WAITER_RADIO_BTN = { xpath: '/html/body/div/main/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div[1]/fieldset/div/label[1]/span[1]/span[1]/input' }.freeze
-  WAITING_FOR_CONFIRM_TAB = { xpath: '/html/body/div/main/div/header/div/div[2]/div[2]/div/button[1]/span[1]/span/span' }.freeze
+  ROW = { xpath: '//div/main/div/div/div/div/div[1]/div[1]/div[1]/div' }.freeze
+  ACCEPT_BTN = { xpath: '//div/main/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div/button/span[1]' }.freeze
+  ASSIGN_BTN = { xpath: '//div/main/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div[3]/button' }.freeze
+  WAITER_RADIO_BTN = { name: 'waiters' }.freeze
+  WAITING_FOR_CONFIRM_TAB = { xpath: '//div/main/div/header/div/div[2]/div[2]/div/button[1]/span[1]/span/span' }.freeze
   ACCEPTED_TAB = { xpath: '//span[contains(text(),"Accepted")]' }.freeze
-  WAITERS_TAB = { xpath: '/html/body/div/main/div/header/div/div[2]/div[2]/div/button[4]/span[1]/span/span' }.freeze
-  WAITER = { xpath: '/html/body/div/main/div/div[1]/div[1]/div[1]/div/div/p' }.freeze
+  WAITERS_TAB = { xpath: '//div/main/div/header/div/div[2]/div[2]/div/button[4]/span[1]/span/span' }.freeze
+  WAITER = { xpath: '//div/main/div/div[1]/div[1]/div[1]/div/div/p' }.freeze
 
   def initialize(driver)
     @driver = driver
@@ -20,7 +20,7 @@ class Administrator
   end
 
   def click_waiter
-    @driver.find_element(WAITER)
+    @driver.find_element(WAITER).click
   end
 
   def click_waiting_for_confirm_tab
@@ -36,26 +36,14 @@ class Administrator
   end
 
   def click_accept_order
-    click_waiting_for_confirm_tab
-    click_order
     @driver.find_element(ACCEPT_BTN).click
   end
 
   def click_assign_order
-    click_accepted_tab
-    click_order
-    @driver.find_element(WAITER_RADIO_BTN).click
     @driver.find_element(ASSIGN_BTN).click
   end
 
-  def click_check_orders_of_waiter
-    click_waiters_tab
-    click_waiter
-  end
-
-  def view_detailed_order_of_waiter
-    click_waiters_tab
-    click_waiter
-    click_order
+  def click_waiter_radio_btn
+    @driver.find_element(WAITER_RADIO_BTN).click
   end
 end
--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -10,5 +10,45 @@ RSpec.describe HomePage do
   let(:user) { cred.fetch('emails')['customer_email'] }
   let(:pw) { cred.fetch('pw')['customer_pwd'] }
 
-  include_context 'login'
+  it 'Restaurant list button redirects to all restaurants' do
+    home.click_restaurants_list
+    @wait.until { @driver.find_element(:xpath, "//div[@title='Johnson PLC']") }
+    expect(@driver.find_element(:xpath, "//div[@title='Johnson PLC']")).to be_displayed
+  end
+
+  it 'Homepage button redirects correctly to homepage' do
+    home.click_homepage
+    @wait.until { @driver.find_element(:xpath, "//img[@alt='View All']") }
+    expect(@driver.find_element(:xpath, "//img[@alt='View All']")).to be_displayed
+  end
+
+  it 'View all button redirects to all restaurants' do
+    home.click_view_all
+    @wait.until { @driver.find_element(:xpath, "//div[@title='Johnson PLC']") }
+    expect(@driver.find_element(:xpath, "//div[@title='Johnson PLC']")).to be_displayed
+  end
+
+  it 'Easy-rest button redirects correctly to homepage' do
+    home.click_easy_rest
+    @wait.until { @driver.find_element(:xpath, "//img[@alt='View All']") }
+    expect(@driver.find_element(:xpath, "//img[@alt='View All']")).to be_displayed
+  end
+
+  it 'Clicking any category redirects to the corresponding category' do
+    home.click_any_category
+    @wait.until { @driver.find_element(:xpath, "//span[normalize-space()='details']") }
+    expect(@driver.find_element(:xpath, "//span[normalize-space()='details']")).to be_displayed
+  end
+
+  it 'Sign in button redirects to log-in page' do
+    home.click_sign_in
+    @wait.until { @driver.find_element(:xpath, "//span[normalize-space()='Google']") }
+    expect(@driver.find_element(:xpath, "//span[normalize-space()='Google']")).to be_displayed
+  end
+
+  it 'Sign up button redirects to sign-up page' do
+    home.click_sign_up
+    @wait.until { @driver.find_element(name: 'phoneNumber') }
+    expect(@driver.find_element(name: 'phoneNumber')).to be_displayed
+  end
 end
